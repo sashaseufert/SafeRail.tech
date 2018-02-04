5af3a65e62c40d807f8747888c2168455c16ebff
Chris Dobson <chris@penguinleaf.com>
2018-02-04 09:53:33 -0500

    Swapped in the video & cleaned things up

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+isMobile = /Mobi/i.test(navigator.userAgent) || /Android/i.test(navigator.userAgent);
+// alert(isMobile);
+if (isMobile) {
+    document.body.className += " isMobile";
+}
 $(document).ready(function () {
     var vid = document.getElementById('paravid'); 
     $(window).scroll(function () {
@@ -10,10 +15,10 @@ $(document).ready(function () {
             if(window.pageYOffset < 2400) $("div#fire .imgContainer").css({ "background-position-y": Math.min(100, Math.max(0, 80 + (window.pageYOffset - $("div#fire .imgContainer").offset().top) / 5)) + "%" });
 
             // console.log("fire:", (10 + ($("body").scrollTop() - $("div#fire .imgContainer").offset().top)) + "%")
-            $(vid).css({ "position": "fixed", "z-index": 10, "top": Math.min(100, 9500 - window.pageYOffset ), "right": Math.min(100, window.pageYOffset*7 - 18000) , "width": "50%"});
+            if (!isMobile) $(vid).css({ "position": "fixed", "z-index": 10, "top": Math.min(100, 9500 - window.pageYOffset ), "right": Math.min(100, window.pageYOffset*7 - 18000) , "width": "50%"});
             if (window.pageYOffset > 2000) {
                 // console.log()
-                var frame = Math.min(vid.duration, (window.pageYOffset - $("div#parallax-container").offset().top) / 150);
+                var frame = Math.min(vid.duration, (window.pageYOffset - $("div#parallax-container").offset().top) / 220);
                 vid.currentTime = frame;
                 console.log(vid.duration, window.pageYOffset - $("div#parallax-container").offset().top);
                 
@@ -31,3 +36,4 @@ $(document).ready(function () {
         })    
     });
 });
+
