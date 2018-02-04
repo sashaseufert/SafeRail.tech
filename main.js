7db455323fc457e6b83b0582a9b6084da40c7ef7
Chris Dobson <chris@penguinleaf.com>
2018-02-03 22:16:59 -0500

    Cleaned up the movements

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ $(document).ready(function () {
             if(window.pageYOffset < 2400) $("div#fire .imgContainer").css({ "background-position-y": Math.min(100, Math.max(0, 80 + (window.pageYOffset - $("div#fire .imgContainer").offset().top) / 5)) + "%" });
 
             // console.log("fire:", (10 + ($("body").scrollTop() - $("div#fire .imgContainer").offset().top)) + "%")
-            $(vid).css({ "position": "fixed", "z-index": 10, "top": 100, "right": Math.min(100, window.pageYOffset*7 - 18000) , "width": "50%"});
+            $(vid).css({ "position": "fixed", "z-index": 10, "top": Math.min(100, 9500 - window.pageYOffset ), "right": Math.min(100, window.pageYOffset*7 - 18000) , "width": "50%"});
             if (window.pageYOffset > 2000) {
                 // console.log()
-                var frame = Math.min(vid.duration, (window.pageYOffset - $("div#parallax-container").offset().top) / 30);
+                var frame = Math.min(vid.duration, (window.pageYOffset - $("div#parallax-container").offset().top) / 150);
                 vid.currentTime = frame;
                 console.log(vid.duration, window.pageYOffset - $("div#parallax-container").offset().top);
                 
